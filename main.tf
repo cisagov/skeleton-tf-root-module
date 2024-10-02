5e9af1aa8ca2b450ff28b12b7d24a0e377872a72
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2024-10-02 15:05:28 -0400

    Move the provider configuration into its own file
    This fits our standard practice.

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-provider "aws" {
-  # Our primary provider uses our terraform role
-  assume_role {
-    role_arn     = var.tf_role_arn
-    session_name = "terraform-example"
-  }
-  default_tags {
-    tags = var.tags
-  }
-  region = var.aws_region
-}
-
 #-------------------------------------------------------------------------------
 # Configure the example module.
 #-------------------------------------------------------------------------------
